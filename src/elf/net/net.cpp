0149df2d418359ce8e2cdde7fb4f0edbbdcf1955
Tuz <youngtrips@gmail.com>
2015-07-02 16:14:50 +0800

    [MOD] backlog && disable linger

--- src/elf/net/net.cpp
+++ src/elf/net/net.cpp
@@ -22,7 +22,7 @@
 #include <string>
 
 namespace elf {
-static const int LINGER_ONOFF = 1;
+static const int LINGER_ONOFF = 0;
 static const int LINGER_TIME = 5;
 static const int CONTEXT_CLOSE_TIME = 300;
 static const int SIZE_INT = sizeof(int(0));
@@ -32,6 +32,7 @@ static const size_t CHUNK_MAX_NUM = 10000;
 static const int MESSAGE_MAX_NAME_LENGTH = 100;
 static const int MESSAGE_MAX_VALID_SIZE = CHUNK_MAX_NUM * CHUNK_DEFAULT_SIZE;
 static const int MESSAGE_MAX_PENDING_SIZE = MESSAGE_MAX_VALID_SIZE * 2;
+static const int BACKLOG = 128;
 
 struct blob_t;
 struct chunk_t;
@@ -654,7 +655,7 @@ int net_listen(oid_t peer, const std::string &name,
         return -1;
     }
 
-    if (0 != listen(s_sock, 0)) {
+    if (0 != listen(s_sock, BACKLOG)) {
         LOG_ERROR("net", "[%s] (%s:%d) listen FAILED: %s.",
                 name.c_str(), ip.c_str(), port,
                 strerror(errno));
