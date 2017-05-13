19b9f9cb5f1728da9d2237a9903258648006a543
Tuz <youngtrips@gmail.com>
2017-05-13 15:08:49 +0800

    [MOD} gen grpc

--- gen.sh
+++ gen.sh
@@ -3,7 +3,13 @@
 INPUT_PATH=proto
 OUTPUT_PATH=src/elf/net
 
-protoc -I . --cpp_out=$OUTPUT_PATH $INPUT_PATH/*.proto
-protoc -I . --grpc_out=$OUTPUT_PATH --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` $INPUT_PATH/*.proto
+PROTOC=
+if [ ! -n "$ENV_PATH" ]; then
+    PROTOC=protoc
+else
+    PROTOC=$ENV_PATH/bin/protoc
+fi
+${PROTOC} -I . --cpp_out=$OUTPUT_PATH $INPUT_PATH/*.proto
+${PROTOC} -I . --grpc_out=$OUTPUT_PATH --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` $INPUT_PATH/*.proto
 
 find  . -name "*.cc" | awk '{print "mv "$1" "$1"x"}' | sed "s/ccx/cpp/g" | bash
