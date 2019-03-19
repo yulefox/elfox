a87672d90d934b3acbbe9554e451073c3deca082
dongcm <youngtrips@gmail.com>
2019-03-19 17:59:46 +0800

    [MOD] generate proto

--- gen.sh
+++ gen.sh
@@ -12,6 +12,6 @@ else
 fi
 ${PROTOC} --version
 ${PROTOC} -I . --cpp_out=$OUTPUT_PATH $INPUT_PATH/*.proto
-${PROTOC} -I . --grpc_out=$OUTPUT_PATH --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` $INPUT_PATH/*.proto
+${PROTOC} -I . --grpc_out=$OUTPUT_PATH --plugin=protoc-gen-grpc=$ENV_PATH/bin/grpc_cpp_plugin $INPUT_PATH/*.proto
 
 find  . -name "*.cc" | awk '{print "mv "$1" "$1"x"}' | sed "s/ccx/cpp/g" | bash
